2c8e11091e08d09ecdb34d093aa197c913b1e3c7
suzanne301 <104364608+suzanne301@users.noreply.github.com>
2024-05-27 01:54:19 +0100

    updated css and html as appropriate for a children's limerick generator

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ function generatePoem(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "03351b3tc2f7fc9oa83ff4ea58bed167";
-  let prompt = `User instructions: Generate a Scottish poem about ${instructionsInput.value}`;
-  let context = "You are a romantic poem expert and love to write short and witty poems.  Your mission is to generate a four line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title with the poem. Sign the poem with 'SheCodes AI' inside a <strong> element underneath the poem.";
+  let prompt = `User instructions: ⏳  Your limerick about ${instructionsInput.value} is on its way!`;
+  let context = "You are a funny limerick expert that enjoys writing limericks for young children aged 10 and under.  Your mission is to generate a five line limerick in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title with the poem. Sign the poem with 'Suzanne Paterson AI' inside a <strong> element underneath the poem.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">⏳  Generating a Scottish poem about ${instructionsInput.value} ...</div>`
+  poemElement.innerHTML = `<div class="generating">⏳  Your limerick about ${instructionsInput.value} is on its way!</div>`
 
   axios.get(apiURL).then(displayPoem);
 }
